404cb55a94b2c0423432679d5733396e3b987aa1
Vaikundamuthu <vkannan@hexalytics.com>
2025-11-25 17:47:34 +0530

    Removed duplicate repositories from code and fixed the testcase

--- src/main/java/com/alsharif/operations/portactivity/repository/PortActivityMasterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alsharif.operations.portactivity.repository;
-
-import com.alsharif.operations.portactivity.entity.PortActivityMaster;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PortActivityMasterRepository extends JpaRepository<PortActivityMaster, Long> {
-}
