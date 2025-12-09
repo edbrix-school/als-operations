41ff98adad3a0359ae35d4daba5eb8359c957afa
Vaikundamuthu <vkannan@hexalytics.com>
2025-12-09 21:35:56 +0530

    Configured repositories and entities for QA environment

--- src/main/java/com/asg/operations/OperationsApplication.java
+++ src/main/java/com/asg/operations/OperationsApplication.java
@@ -2,10 +2,20 @@ package com.asg.operations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.asg"})
+@EnableJpaRepositories(basePackages = {
+		"com.asg.operations",
+		"com.asg.common.lib.repository"
+})
+@EntityScan(basePackages = {
+		"com.asg.operations",
+		"com.asg.common.lib.entity"
+})
 public class OperationsApplication {
 
 	public static void main(String[] args) {
