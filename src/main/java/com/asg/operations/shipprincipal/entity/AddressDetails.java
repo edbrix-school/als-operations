e80dc860278cc25e49a7e7a12305d92fa7e8b68e
mjainhexa <mjain@hexalytics.com>
2026-02-03 13:18:09 +0530

    fix/re-check

--- src/main/java/com/asg/operations/shipprincipal/entity/AddressDetails.java
+++ src/main/java/com/asg/operations/shipprincipal/entity/AddressDetails.java
@@ -16,6 +16,7 @@ import java.time.LocalDateTime;
 public class AddressDetails {
     @Id
     @Column(name = "ADDRESS_POID", nullable = false)
+    @AuditIgnore
     private BigDecimal addressPoid;
 
     @Column(name = "ADDRESS_MASTER_POID", nullable = false)
--- src/main/java/com/asg/operations/shipprincipal/service/PrincipalMasterServiceImpl.java
+++ src/main/java/com/asg/operations/shipprincipal/service/PrincipalMasterServiceImpl.java
@@ -510,6 +510,8 @@ public class PrincipalMasterServiceImpl implements PrincipalMasterService {
                     entity.setRemarks(charge.getRemarks());
                     entity.setCreatedDate(LocalDateTime.now());
                     chargeRepository.save(entity);
+                    String logDetail = String.format("Row Created on Principal Charge Detail with detRowId: %s", nextDetRowId);
+                    loggingService.createLogSummaryEntry(UserContext.getDocumentId(), id.toString(), logDetail);
                 } else if (action == ActionType.isUpdated) {
                     chargeRepository.findById(new ShipPrincipalMasterDtlId(id, charge.getDetRowId()))
                             .ifPresent(existing -> {
@@ -525,6 +527,7 @@ public class PrincipalMasterServiceImpl implements PrincipalMasterService {
                             });
                 } else if (action == ActionType.isDeleted) {
                     chargeRepository.deleteById(new ShipPrincipalMasterDtlId(id, charge.getDetRowId()));
+                    loggingService.logDelete(charge, UserContext.getDocumentId(), id.toString());
                 }
             }
         }
@@ -541,6 +544,8 @@ public class PrincipalMasterServiceImpl implements PrincipalMasterService {
                     mapper.mapPaymentDTOToEntity(payment, entity);
                     entity.setCreatedDate(LocalDateTime.now());
                     paymentRepository.save(entity);
+                    String logDetail = String.format("Row Created on Principal Payment Detail with detRowId: %s", nextDetRowId);
+                    loggingService.createLogSummaryEntry(UserContext.getDocumentId(), id.toString(), logDetail);
                 } else if (action == ActionType.isUpdated) {
                     paymentRepository.findById(new ShipPrincipalMasterDtlId(id, payment.getDetRowId()))
                             .ifPresent(existing -> {
@@ -554,6 +559,7 @@ public class PrincipalMasterServiceImpl implements PrincipalMasterService {
                             });
                 } else if (action == ActionType.isDeleted) {
                     paymentRepository.deleteById(new ShipPrincipalMasterDtlId(id, payment.getDetRowId()));
+                    loggingService.logDelete(payment, UserContext.getDocumentId(), id.toString());
                 }
             }
         }
@@ -596,6 +602,8 @@ public class PrincipalMasterServiceImpl implements PrincipalMasterService {
                     entity.setCreatedBy(user.getUserName());
                     entity.setCreatedDate(LocalDateTime.now());
                     paRptDtlRepository.save(entity);
+                    String logDetail = String.format("Row Created on Principal Port Activity Report Detail with detRowId: %s", nextDetRowId);
+                    loggingService.createLogSummaryEntry(UserContext.getDocumentId(), id.toString(), logDetail);
                 } else if (action == ActionType.isUpdated) {
                     paRptDtlRepository.findById(new ShipPrincipalPaRptDtlId(id, paRptDetail.getDetRowId()))
                             .ifPresent(existing -> {
@@ -619,6 +627,7 @@ public class PrincipalMasterServiceImpl implements PrincipalMasterService {
                             });
                 } else if (action == ActionType.isDeleted) {
                     paRptDtlRepository.deleteById(new ShipPrincipalPaRptDtlId(id, paRptDetail.getDetRowId()));
+                    loggingService.logDelete(paRptDetail, UserContext.getDocumentId(), id.toString());
                 }
             }
         }
