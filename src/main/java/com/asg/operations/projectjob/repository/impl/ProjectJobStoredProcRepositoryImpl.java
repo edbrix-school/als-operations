4d0443b76f22e71d2b20b09c4bcd5ac7ff94bbeb
Sridharan-stack <srajendaran@hexalytics.com>
2026-03-09 11:36:18 +0530

    Update the Api response for the load job

--- src/main/java/com/asg/operations/projectjob/repository/impl/ProjectJobStoredProcRepositoryImpl.java
+++ src/main/java/com/asg/operations/projectjob/repository/impl/ProjectJobStoredProcRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.asg.operations.projectjob.dto.ProjectLoadDtlRow;
 import com.asg.operations.projectjob.dto.ProjectLoadHdrRow;
 import com.asg.operations.projectjob.dto.ProjectLoadInJobsProcResponse;
 import com.asg.operations.projectjob.repository.ProjectJobStoredProcRepository;
+import io.micrometer.common.util.StringUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -50,7 +51,8 @@ public class ProjectJobStoredProcRepositoryImpl implements ProjectJobStoredProcR
                             row.setShipmentMode(rs.getString("SHIPMENT_MODE"));
                             row.setTransportationMode(rs.getString("TRANSPORTATION_MODE"));
                             row.setProjectReference(rs.getString("PROJECT_REFERENCE"));
-                            row.setCommodity(rs.getString("COMMODITY"));
+                            String commodityPoids = rs.getString("COMMODITY");
+                            row.setCommodityPoids(StringUtils.isEmpty(commodityPoids) ? null : List.of(commodityPoids.split((","))));
                             hdrList.add(row);
                         }
                     }
