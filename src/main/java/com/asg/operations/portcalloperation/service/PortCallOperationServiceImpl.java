fa98254e96001b1b55c2359834b3d1af4127bcbe
Vaikundamuthu <vkannan@hexalytics.com>
2026-05-07 15:45:47 +0530

    mapped mail details in berthing get api

--- src/main/java/com/asg/operations/portcalloperation/service/PortCallOperationServiceImpl.java
+++ src/main/java/com/asg/operations/portcalloperation/service/PortCallOperationServiceImpl.java
@@ -1643,6 +1643,18 @@ public class PortCallOperationServiceImpl implements PortCallOperationService {
                     .ifPresent(emailRecord -> {
                         builder.emailSentOn(emailRecord.getEmailSendOn() != null ? emailRecord.getEmailSendOn().atStartOfDay() : null).remarks(emailRecord.getEmailRemarks());
                     });
+
+            List<PortCallOperationDocsMsgsDtl2> docsMsgsDtl2Details = docsMsgsDtl2Repository.findByEmailPoid(entity.getEmailPoid());
+            List<PortCallOperationMailDetailResponseDto> mailDetails = docsMsgsDtl2Details.stream().map(msgDtl2 -> PortCallOperationMailDetailResponseDto.builder()
+                    .transactionPoid(msgDtl2.getTransactionPoid())
+                    .detRowId(msgDtl2.getDetRowId())
+                    .addressee(msgDtl2.getAddressee())
+                    .company(msgDtl2.getCompany())
+                    .emailIds(msgDtl2.getToEmailId())
+                    .emailIdsCC(msgDtl2.getCcEmailId())
+                    .communicationType(msgDtl2.getEmailType())
+                    .build()).collect(Collectors.toList());
+            builder.mailDetails(mailDetails);
         }
 
         return builder.build();
