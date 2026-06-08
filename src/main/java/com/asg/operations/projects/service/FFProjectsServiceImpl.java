f5a33a026090ad7f0ae66f56afbbc4f7a3259366
mjainhexa <mjain@hexalytics.com>
2026-06-08 16:52:31 +0530

    fix/ASAS-4076 Fixed det row issue

--- src/main/java/com/asg/operations/projects/service/FFProjectsServiceImpl.java
+++ src/main/java/com/asg/operations/projects/service/FFProjectsServiceImpl.java
@@ -439,7 +439,7 @@ public class FFProjectsServiceImpl implements FFProjectsService {
                 .map(cs -> mapToUpcomingJobDTO(cs, manifestById.get(cs.getJobNoPoid())))
                 .collect(Collectors.toList());
         applySorting(result, sortBy, sortDir);
-        assignDetRowIds(result, UpcomingJobDTO::setDetRowId);
+//        assignDetRowIds(result, UpcomingJobDTO::setDetRowId);
         return result;
     }
 
